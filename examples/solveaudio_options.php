77158fba7ec5bea205252d776e2864c0bdd755fe
zatvorius <zatvorius@gmail.com>
2023-02-27 12:26:40 +0400

    Add files via upload

---
+++ examples/solveaudio_options.php
@@ -0,0 +1,21 @@
+<?php
+
+set_time_limit(130);
+
+require(__DIR__ . '/../src/autoloader.php');
+
+$solver = new \TwoCaptcha\TwoCaptcha([
+    'apiKey'	=> 'YOUR_API_KEY',
+    'server'	=> 'http://2captcha.com'
+]);
+
+try {
+    $result = $solver->solveaudio([
+        'body' => base64_encode(file_get_contents(__DIR__ . '/audio/audio-ru.mp3')),
+        'lang'     => 'ru',
+    ]);
+} catch (\Exception $e) {
+    die($e->getMessage());
+}
+
+die('Captcha solved: ' . $result->code);
