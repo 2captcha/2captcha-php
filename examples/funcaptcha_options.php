89bf887e57ef6035a04d0db46c853672da4d213a
dzmitry-duboyski <dzmitry.duboyski@gmail.com>
2024-11-22 19:41:23 +0400

    Update example

--- examples/funcaptcha_options.php
+++ examples/funcaptcha_options.php
@@ -15,9 +15,7 @@ try {
         'url'       => 'https://mysite.com/page/with/funcaptcha',
         'surl'      => 'https://client-api.arkoselabs.com',
         'userAgent' => 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.61 Safari/537.36',
-        'data'      => [
-            'anyKey' => 'anyStringValue',
-        ],
+        'data'      =>  '{"anyKey":"anyStringValue"}',
         'proxy'   => [
             'type' => 'HTTPS',
             'uri'  => 'login:password@IP_address:PORT',
