dda6cc9d5b8cda7a814bfd3183c9fa87ee7e4350
dzmitry-duboyski <dzmitry.duboyski@gmail.com>
2024-11-25 16:14:12 +0400

    Update FunCaptchaTest

--- tests/FunCaptchaTest.php
+++ tests/FunCaptchaTest.php
@@ -13,9 +13,7 @@ class FunCaptchaTest extends AbstractWrapperTestCase
             'url'       => 'https://mysite.com/page/with/funcaptcha',
             'surl'      => 'https://client-api.arkoselabs.com',
             'userAgent' => 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.61 Safari/537.36',
-            'data'      => [
-                'anyKey' => 'anyStringValue',
-            ],
+            'data'      =>  '{"anyKey":"anyStringValue"}',
             'proxy'     => [
                 'type' => 'HTTPS',
                 'uri'  => 'username:str0ngP@$$W0rd@1.2.3.4:4321',
@@ -28,9 +26,7 @@ class FunCaptchaTest extends AbstractWrapperTestCase
             'pageurl'   => 'https://mysite.com/page/with/funcaptcha',
             'surl'      => 'https://client-api.arkoselabs.com',
             'userAgent' => 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.61 Safari/537.36',
-            'data'      => [
-                'anyKey' => 'anyStringValue',
-            ],
+            'data'      =>  '{"anyKey":"anyStringValue"}',
             'proxy'     => 'username:str0ngP@$$W0rd@1.2.3.4:4321',
             'proxytype' => 'HTTPS',
             'soft_id'   => '4585',
