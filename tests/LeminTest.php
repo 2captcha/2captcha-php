7c5b92cd339ed35d3c1dd3d8aac19b4160c0b99f
victorkowalski <vkowalski@yandex.ru>
2025-09-19 09:06:29 +0300

    json param added

--- tests/LeminTest.php
+++ tests/LeminTest.php
@@ -26,6 +26,7 @@ class LeminTest extends AbstractWrapperTestCase
             'proxy'      => 'username:str0ngP@$$W0rd@1.2.3.4:4321',
             'proxytype'  => 'HTTPS',
             'soft_id'    => '4585',
+            'json'       => '0'
         ];
 
         $this->checkIfCorrectParamsSendAndResultReturned([
