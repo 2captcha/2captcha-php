7c7562c42044aae4d6740279a86fa7f7f98d6747
victorkowalski <vkowalski@yandex.ru>
2025-11-07 08:25:16 +0300

    vkcaptcha upd

--- examples/vk_captcha.php
+++ examples/vk_captcha.php
@@ -12,7 +12,6 @@ $solver = new TwoCaptcha($argv[1]);
 try {
 
     $tokenBasedResult = $solver->vkCaptcha([
-        'method' => 'vkcaptcha',
         'redirect_uri' => 'https://id.vk.com/not_robot_captcha?domain=vk.com&session_token=eyJ....HGsc5B4LyvjA&variant=popup&blank=1',
         'userAgent' => 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36',
         'proxy' => [
