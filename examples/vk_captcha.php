3d3305bc5f86484d4ac1e1ad16cdb7ecf13c533a
victorkowalski <vkowalski@yandex.ru>
2025-11-07 08:13:58 +0300

    refactoring

--- examples/vk_captcha.php
+++ examples/vk_captcha.php
@@ -10,14 +10,7 @@ require(__DIR__ . '/../src/autoloader.php');
 $solver = new TwoCaptcha($argv[1]);
 
 try {
-    //image-based
-    $result = $solver->vkImage([
-        'body' => base64_encode(file_get_contents(__DIR__ . '/images/vk.jpg')),
-        'steps' => '[5,12,22,24,21,23,10,7,2,8,19,18,8,24,21,22,11,14,16,5,18,20,4,21,12,6,0,0,11,12,8,20,19,3,14,8,9,13,16,24,18,3,2,23,8,12,6,1,11,0,20,15,19,22,17,24,8,0,12,5,19,14,11,6,7,14,23,24,23,20,4,20,6,12,4,17,4,18,6,20,17,5,23,7,10,2,8,9,5,4,17,24,11,14,4,10,12,22,21,2]',
-    ]);
 
-    //token-based
-    /*
     $tokenBasedResult = $solver->vkCaptcha([
         'method' => 'vkcaptcha',
         'redirect_uri' => 'https://id.vk.com/not_robot_captcha?domain=vk.com&session_token=eyJ....HGsc5B4LyvjA&variant=popup&blank=1',
@@ -29,7 +22,6 @@ try {
     ]);
 
     echo 'TokenBasedResult Captcha solved: ' . $tokenBasedResult->code;
-    */
 
 } catch (\Exception $e) {
     die($e->getMessage());
